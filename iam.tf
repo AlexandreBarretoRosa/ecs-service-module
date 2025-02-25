f19d925ecdef5d4c00f5a9de4ee4e3a21d022e8f
Alexandre Barreto <lillico1405@gmail.com>
2025-02-24 23:47:42 -0300

    criando modulo service ecs

---
+++ iam.tf
@@ -0,0 +1,47 @@
+resource "aws_iam_role" "service_execution_role" {
+    name = format("%s-%s-service-role", var.cluster_name, var.service_name)
+
+     assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    "Name" = "service role"
+  }
+}
+  
+resource "aws_iam_role_policy" "service_execution_role" {
+  name = format("%s-%s-service-role", var.cluster_name, var.service_name)
+  role = aws_iam_role.service_execution_role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+          "elasticloadbalancing:DeregisterTargets",
+          "elasticloadbalancing:Describe*",
+          "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+          "elasticloadbalancing:RegisterTargets",
+          "ec2:Describe*",
+          "ec2:AuthorizeSecurityGroupIngress",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
